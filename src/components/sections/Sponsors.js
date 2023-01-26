3251fa190f278b51e559385ef9d0e11d24eaf34f
Subramanian-Ria <riasub08@gmail.com>
2023-01-26 14:38:38 -0500

    Mack Institute Sponsor

--- src/components/sections/Sponsors.js
+++ src/components/sections/Sponsors.js
@@ -61,9 +61,9 @@ const Sponsors = () => (
               <a href="https://www.bloomberg.com/company/engineering/"><img width="250px" src={BloombergLogo} /></a>
     <a href="https://www.nerdwallet.com/?bucket_id=dt-hp&ds_rl=1246084&gclid=Cj0KCQiAgP6PBhDmARIsAPWMq6kU2lzF2hxIw7n67CEmqEmVPSZo0XmFuUD7hH8AlVhmSiV_zAweWEcaAoMBEALw_wcB&gclsrc=aw.ds&mktg_body=1678&mktg_hline=19498&mktg_img=3598&mktg_place=kwd-40958987563%7D&model_execution_id=DB0B89FD-876C-4011-83E1-4177EB33C3EC&nw_campaign_id=150238270218585900&utm_campaign=cc_mktg_paid_060716_brand_exact&utm_content=ta&utm_medium=cpc&utm_source=goog&utm_term=nerdwallet"> <img width="260px" src={NWLogo} /> </a> */}
               <a href="https://www.alumni.upenn.edu/s/1587/gid2/16/interior_1col.aspx?sid=1587&gid=2&pgid=420"> <img className="sponsor-logo-class" src={TCPWLogo} /> </a>
-    {/*<a href="https://mackinstitute.wharton.upenn.edu"><img width="230px" src={MackLogo} /></a>*/}
               <a href="https://www.janestreet.com/"><img width="230px" src={JaneStreetLogo} /></a>
               <a href="https://www.linode.com/"><img width="230px" src={LinodeLogo} /></a>
+              <a href="https://mackinstitute.wharton.upenn.edu"><img width="230px" src={MackLogo} /></a>
               <a href="https://cloud.google.com"> <img width="350" src={GCPLogo} /> </a>
               {/* <a href="https://www.mckinsey.com/careers/home"><img width="230px" src={MckLogo} /></a> */}
               {/* <a href="https://www.twilio.com/company/jobs"><img width="230px" src={TwilioLogo} /></a> */}
